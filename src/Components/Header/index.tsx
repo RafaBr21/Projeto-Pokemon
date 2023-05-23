433c69d8753e481126d348637c07898d4472530b
Rafael Borges da Silva <rafael.borges123@hotmail.com>
2023-05-23 14:06:21 -0300

    implementando Header na home

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/Logo.svg'
+import styles from './index.module.css'
+
+export const Header = () => {
+    return (
+        <header className={styles.Section_Header}>
+            <nav className={styles.Nav}>
+                <img className={styles.img} src={logo} alt="Imagem do logo principal" />
+                <article className={styles.content_Nav}>
+                    <a className={styles.Nav_item} href="#">Home</a>
+                    <a href="#">Pokédex</a>
+                    <a href="#">Legendaries</a>
+                    <a href="#">Documentation</a>
+                </article>
+            </nav>
+        </header>
+    )
+}
