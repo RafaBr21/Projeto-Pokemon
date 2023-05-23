6dbcc7f8353a5a14610967af7855537aa7ba2fac
Rafael Borges da Silva <rafael.borges123@hotmail.com>
2023-05-23 16:18:42 -0300

    implementando conteudo na Home

---
+++ src/Components/Home/index.tsx
@@ -0,0 +1,18 @@
+import styles from './index.module.css'
+import BannerComplete from '../../assets/banner.png'
+
+export const Home = () => {
+    return (
+        <section className={styles.Section_Home}>
+
+            <article className={styles.article_Content}>
+                <h1 className={styles.title}> <strong>Find</strong> all your 
+                    favorite <strong>Pokemon</strong></h1>
+                <h2 className={styles.title_two}>
+                    You can know the type of Pokemon, its strengths, disadvantages and abilities</h2>
+                <button className={styles.button}>See pokemons</button>
+            </article>
+            <img className={styles.img} src={BannerComplete} alt="Imagem Completa do personagem Pikachu" />
+        </section>
+    )
+}
